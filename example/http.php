e3e98433a205cb7ecf9e77460ea5c0718b592c29
吕海涛 <i@lvht.net>
2017-12-13 01:18:45 +0800

    use channel for multi request

--- example/http.php
+++ example/http.php
@@ -2,9 +2,30 @@
 require __DIR__.'/../vendor/autoload.php';
 
 use Fiber\Helper as f;
+use Fiber\Util\Channel;
 
 f\once(function () {
-    $http = \Fiber\Http\Client::create(['base_uri' => 'http://myip.ipip.net']);
-    $response = $http->request('GET', '/');
-    echo $response->getBody();
+    $c = new Channel;
+
+    f\go(function () use($c) {
+        $http = \Fiber\Http\Client::create(['base_uri' => 'http://127.0.0.1:8080']);
+        $response = $http->request('GET', '/');
+        $c->write($response->getBody());
+    });
+
+    f\go(function () use($c) {
+        $http = \Fiber\Http\Client::create(['base_uri' => 'http://127.0.0.1:8081']);
+        $response = $http->request('GET', '/');
+        $c->write($response->getBody());
+    });
+
+    f\go(function () use($c) {
+        $http = \Fiber\Http\Client::create(['base_uri' => 'http://127.0.0.1:8082']);
+        $response = $http->request('GET', '/');
+        $c->write($response->getBody());
+    });
+
+    echo $c->read();
+    echo $c->read();
+    echo $c->read();
 });
